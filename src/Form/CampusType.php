891796a8ab1f221326846c10b9710bda68e8665b
Dostix <francoisp37@live.fr>
2020-10-13 10:43:03 +0200

    petit commit avec remove et debut campus2

--- src/Form/CampusType.php
+++ src/Form/CampusType.php
@@ -4,7 +4,30 @@
 namespace App\Form;
 
 
+use App\Entity\C;
+use App\Entity\Campus;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
 class CampusType
 {
 
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom',TextType::class,['label' => 'Nom']);
+    }
+
+
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Campus::class,
+        ]);
+    }
 }
