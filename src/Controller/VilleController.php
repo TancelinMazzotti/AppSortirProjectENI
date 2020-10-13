9f16b3706691c8a762cc00ad3cd94bc4c9da6c22
ilan <glet.ilan.56@gmail.com>
2020-10-13 09:44:23 +0200

    création du controlleur ville et du template ville

---
+++ src/Controller/VilleController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/Ville", name="ville_")
+ * Class VilleController
+ * @package App\Controller
+ */
+class VilleController extends AbstractController
+{
+    /**
+     * @Route("/", name="ville")
+     */
+    public function getVilleList()
+    {
+        return $this->render('ville/index.html.twig', [
+            'title' => 'Ville',
+        ]);
+    }
+}
